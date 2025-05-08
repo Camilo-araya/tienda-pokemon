60d3acea19f119a4b8e599f52a536115727d33e6
Camilo Araya Olivera <camilohilda10@gmail.com>
2025-05-08 16:53:16 -0400

    Actualizada base en vite.config.js y nuevo build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/[nombre-de-tu-repositorio]/',
+  base: 'https://github.com/Camilo-araya/tienda-pokemon',
 });
