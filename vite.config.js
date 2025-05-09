55adb823ba3147392fbbc1b9a5dc4b6323ae5b5f
Camilo Araya Olivera <camilohilda10@gmail.com>
2025-05-09 01:09:46 -0400

    Añadido build.outDir explícito en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante: usar './' para rutas relativas
+  base: './',
+  build: {
+    outDir: 'dist', // Aseguramos que la salida del build sea la carpeta 'dist'
+  },
 });
